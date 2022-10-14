3219f74bf20e10e62e566f7b7148bb46c63e91a8
cuongnew87 <cuongnew37@gmail.com>
2022-10-14 14:40:39 +0700

    add Home page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
+    <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 export default {
-  name: "app",
-  components: {},
+  components: {
+    Header,
+    Footer
+  },
   data() {
     return {};
   },
